b6418a12ea2e619d1bfd4b2eab2f3f53e410a10a
Anik <anik.khondokar09@gmail.com>
2026-05-27 11:33:17 +0600

    SINGLE DIMENTIONAL (EVEN / ODD) PARITY

--- Lab 1 _ Feb 18/src/Problem01.java
+++ Lab 1 _ Feb 18/src/Problem01.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Problem01 {
+    public static void main(String[] args) {
+
+        //The scanner for taking input from the user.
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the binary strings:");
+        String data=scanner.next();
+
+
+        System.out.println("Enter the choice:");
+        System.out.println("(1 for even parity and 2 for odd parity)");
+        int choice=scanner.nextInt();
+
+        //Count the number of '1' in the string.
+        int count=0;
+        for(int i=0; i<data.length(); i++){
+            if( data.charAt(i)=='1'){
+                count++;
+            }
+        }
+
+        //Generate a parity string
+        String result;
+
+        if(choice==1){
+            if(count%2==0){
+                result=data+'0';
+            }else {
+                result=data+'1';
+            }
+        }else {
+            if(count%2!=0){
+                result=data+'0';
+            }else {
+                result=data+'1';
+            }
+        }
+
+        //Output:
+        System.out.println("The String after adding parity bits is : "+result);
+    }
 }
